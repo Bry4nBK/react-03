f96a38b0416f1781ecda33e015c5b711ccc5b03d
Bry4nBK <boork.lobo@gmail.com>
2024-06-09 17:59:45 -0400

    subir proyecto

---
+++ src/BaseColaboradores.js
@@ -0,0 +1,32 @@
+const BaseColaboradores = [
+    {
+      id: "1",
+      nombre: "Juan Pérez",
+      correo: "juan.perez@example.com",
+      edad: 30,
+      cargo: "Desarrollador",
+      telefono: "123456789",
+      experiencia: 3 
+    },
+    {
+      id: "2",
+      nombre: "María Gómez",
+      correo: "maria.gomez@example.com",
+      edad: 25,
+      cargo: "Diseñadora",
+      telefono: "987654321",
+      experiencia: 1 
+    },
+    {
+      id: "3",
+      nombre: "Carlos Ramírez",
+      correo: "carlos.ramirez@example.com",
+      edad: 28,
+      cargo: "Gerente de Proyecto",
+      telefono: "456123789",
+      experiencia: 2 
+    }
+  ];
+  
+  export default BaseColaboradores;
+  
